40f68f02ea6edc1213e38abddb58efb3ea103c40
dniketh <dniketh@actalentservices.com>
2024-09-03 12:01:39 +0000

    Added the NavBar and SearchBar

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,24 @@
+import { Button } from "@/components/ui/button"
+import {
+  Avatar,
+  AvatarFallback,
+  AvatarImage,
+} from "@/components/ui/avatar"
+const NavBar = () => {
+  return (
+    <div className="border-2 flex justify-between h-16 items-center font-bold" >
+        <h1 className="text-3xl ml-4">Book My Venue</h1>
+        <div className="flex">
+        <Button variant="outline" className="mr-10 hover:shadow-sm">Bookings</Button>
+        <Button variant="outline" className="mr-10 hover:shadow-sm">Logout</Button>
+        {/* <Button variant="outline" className="mr-10 rounded-full hover:bg-slate-300">P</Button> */}
+        <Avatar className="mr-4 hover:cursor-pointer">
+      <AvatarImage src="https://github.com/shadcn.png" alt="user/provider profile " />
+      <AvatarFallback>CN</AvatarFallback>
+    </Avatar>
+        </div>
+    </div>
+  )
+}
+
+export default NavBar
---
+++ src/components/SearchBar.tsx
@@ -0,0 +1,13 @@
+import { Button } from './ui/button'
+import { Input } from './ui/input'
+
+const SearchBar = () => {
+  return (
+    <div className="flex w-full max-w-sm items-center space-x-2">
+    <Input type="email" placeholder="Search for a venue" />
+    <Button type="submit">Search</Button>
+  </div>
+  )
+}
+
+export default SearchBar
