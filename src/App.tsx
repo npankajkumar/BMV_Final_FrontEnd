aebcc005be9061fa08cbf8c23a807149107d99b6
dniketh <dniketh@actalentservices.com>
2024-09-06 12:35:15 +0000

    Designed the dashboard components and grid

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,11 @@
+
 import NavBar from "./components/NavBar";
-import RegisterVenue from "./components/RegisterVenue";
-import { DataTable } from "./components/data-table/DataTable";
-import { columns } from "./components/data-table/columns";
-import { availableSlots } from "./db";
 
 function App() {
   return (
     <div className="w-full h-svh">
       <NavBar />
-      <div className="m-5">
-        <DataTable columns={columns} data={availableSlots}/>
-      </div>
+      
     </div>
   );
 }
